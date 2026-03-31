d276a706009bc6930a468651c780b139661c9e1e
hexplor <hexplor@hexplor.com>
2026-03-31 20:03:44 +0200

    maj de code

--- C/colleen.c
+++ C/colleen.c
@@ -2,8 +2,8 @@
 /*	Le commentaire en dehores du programme	*/
 
 void	the_fonction_needed(void){
-	//BEGIN
-	//END
+	const char* str = "#include <stdio.h>\n/*\tLe commentaire en dehores du programme */\n\nvoid\tthe_fonction_needed(void){\n\tconst char* str = %c%s%c\n\tprintf(str, 34, str, 34);\n\treturn;\n}\n\nint\tmain(void){\n/*	LA FONCTION PRINCIPALE	*/\n\tthe_fonction_needed();\n\treturn 0;\n}\n";
+	printf(str, 34, str, 34);
 	return;
 }
 
