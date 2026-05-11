e10375126d91f56d9686b1af201a8ff22c24199d
hexplor <hexplor@hexplor.com>
2026-05-11 20:38:29 +0200

    update

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -4,14 +4,17 @@ global main
 extern printf
 
 section .data
-	src db "La source a venir"
+	src db "La source a venir", 10, 0
 
 section .text
 
 main:
 	call affiche_source
-
+	ret 
 affiche_source:
       lea rdi, [src]     ; 1er argument: format
       ;lea rsi, [name]    ; 2e argument: %s
       call printf
+      ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
